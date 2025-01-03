8437b4d625aac0e038de1d439111770023cdbfd8
Niranjan Prajapati <81921820+prajapatiniranjan@users.noreply.github.com>
2025-01-03 12:26:19 +0530

    usecontext or createcontext

---
+++ 20th class/ContextProjects/src/Child5.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Child5() {
+  
+    let ordata 
+  
+    return (
+    <div>Child5</div>
+  )
+}
---
+++ 20th class/ContextProjects/src/Child6.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Child6() {
+  return (
+    <div>Child6</div>
+  )
+}
